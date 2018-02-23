73b53c17274d4f6392076f0c5214fbb6582fe1d1
NargizOM <nargizom@code.edu.az>
2018-02-23 20:59:28 +0400

    second attempt

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var menimKassam = new Kassa();
+            var menimKassam2 = new Kassa();
+            
+            menimKassam.BorcVer(200);
+            menimKassam2.BorcAl(700);  
+        }
+    }
+
+    class Kassa {
+        public string KassaAdi = "Menim heyatim";
+        private int PulMiqdari=1000;
+        public void BorcVer(int VerilecekPulMiqdari)
+        {
+            Console.WriteLine("Hesabdaki pul miqdari: "+PulMiqdari );
+            PulMiqdari = PulMiqdari - VerilecekPulMiqdari;
+            Console.WriteLine(VerilecekPulMiqdari+"AZN borc verildikden sonra hesabdaki pul miqdari :"+ PulMiqdari);
+        }
+
+        public void BorcAl (int AlinacaqPulMiqdari)
+        {
+            Console.WriteLine("Hesabdaki pul miqdari;" + PulMiqdari);
+            PulMiqdari = PulMiqdari + AlinacaqPulMiqdari;
+            Console.WriteLine(AlinacaqPulMiqdari + "AZN borc alindiqdan sonra hesabdaki pul miqdari :" + PulMiqdari);
+        }
+
+            };
+
+
+        }
+
+
+           
+
+    
+
+
+
+
